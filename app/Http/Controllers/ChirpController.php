bebeda5ab9b26ee4c4e5070e7d143535f9f5d1cb
DrecQ <drecquist@gmail.com>
2024-12-07 11:45:10 +0100

    Implementation de la methode store, mise en place de la gestion de la relation à plusieurs, protection contre l'affectation de masse

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response; 
 use Illuminate\View\View;
+
 
 
 class ChirpController extends Controller
@@ -34,9 +36,16 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->chirps()->create($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
-    //
+    //Affectation en masse
+    protected $fillable = [
+        'message',
+    ];
 }
